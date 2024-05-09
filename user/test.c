896253e0387e581dde14c0cf379444a7ebab77fc
Dmitriy <dmitriysidorcool@gmail.com>
2024-05-09 11:54:55 +0300

    %^%

--- user/test.c
+++ user/test.c
@@ -5,8 +5,6 @@
 #include "kernel/fs.h"
 #include "kernel/fcntl.h"
 
-#define MAXFILE2 MAXFILE
-
 static uint64 next = 1;
 
 uint64 rand(void) {
@@ -21,26 +19,34 @@ void srand(uint64 seed) {
 #define BUFSIZE (BSIZE / sizeof(uint64))
 uint64 buf[BUFSIZE];
 
-int main() {
+int main(int argc, char* argv[]) {
+  if (argc < 3) {
+  	fprintf(2, "utilite must have 2 arguments!\n");
+  	exit(1);
+  }
+
+  uint64 seed = atoi(argv[1]);
+  uint64 size = atoi(argv[2]);
+  
   printf("bigfile test is running!\n");
   int fd = open("big", O_CREATE|O_RDWR);
   if(fd < 0){
-    printf("error: creat big failed!\n");
+    fprintf(2, "error: creat big failed!\n");
     exit(1);
   }
 
   int cnt = 0;
-  srand(1);
-  for(int i = 0; i < MAXFILE2 * BUFSIZE; ++i){
+  srand(seed);
+  for(int i = 0; i < size * BUFSIZE; ++i){
     rand();
     buf[cnt++] = next;
     cnt %= BUFSIZE;
     if(!cnt) {	
       if (write(fd, buf, BSIZE) != BSIZE){
-      	printf("error: write big file failed!\n");
+      	fprintf(2, "error: write big file failed!\n");
       	exit(1);
       }
-      else if (i / BUFSIZE % 1000 == 0) printf("%d/%d block writed!\n", i / BUFSIZE, MAXFILE2);
+      else if (i / BUFSIZE % 1000 == 0) printf("%d/%d blocks writed!\n", i / BUFSIZE, size);
     }
   }
 
@@ -48,20 +54,19 @@ int main() {
 
   fd = open("big", O_RDONLY);
   if(fd < 0){
-    printf("error: open big failed!\n");
-    exit(1);
+    fprintf(2, "error: open big failed!\n");
+    exit(seed);
   }
 
   cnt = 0;
   srand(1);
-  for(int i = 0; i < MAXFILE2; ++i){
+  for(int i = 0; i < size; ++i){
     if (read(fd, buf, BSIZE) != BSIZE){
- 	  printf("error: write big file failed\n", i);
+ 	  fprintf(2, "error: write big file failed\n", i);
       exit(1);
     } else {
       for (int j = 0; j < BUFSIZE; ++j) {
       	rand();
-      	//printf("%d == %d\n", next, buf[j]);
         if (next != buf[j]) {
           printf("status: test failed!\n");
           exit(0);
